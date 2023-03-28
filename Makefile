26716d62444172c047d553fb7c1a1a59d9fd281f
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2023-03-28 15:16:39 +0700

    feat: add get replay list on handler

--- Makefile
+++ Makefile
@@ -5,7 +5,7 @@ NAME = "github.com/goto/optimus"
 LAST_COMMIT := $(shell git rev-parse --short HEAD)
 LAST_TAG := "$(shell git rev-list --tags --max-count=1)"
 OPMS_VERSION := "$(shell git describe --tags ${LAST_TAG})-next"
-PROTON_COMMIT := "5b1f4b4fc161c3d410d03f00c78f356ebf2dd722"
+PROTON_COMMIT := "124aaf1f3eea4f6f9a97f03416481a63f4308d93"
 
 
 .PHONY: build test test-ci generate-proto unit-test-ci integration-test vet coverage clean install lint
--- protos/gotocompany/optimus/core/v1beta1/replay_grpc.pb.go
+++ protos/gotocompany/optimus/core/v1beta1/replay_grpc.pb.go
@@ -23,6 +23,7 @@ const _ = grpc.SupportPackageIsVersion7
 // For semantics around ctx use and closing/ending streaming RPCs, please refer to https://pkg.go.dev/google.golang.org/grpc/?tab=doc#ClientConn.NewStream.
 type ReplayServiceClient interface {
 	Replay(ctx context.Context, in *ReplayRequest, opts ...grpc.CallOption) (*ReplayResponse, error)
+	ListReplay(ctx context.Context, in *ListReplayRequest, opts ...grpc.CallOption) (*ListReplayResponse, error)
 }
 
 type replayServiceClient struct {
@@ -42,11 +43,21 @@ func (c *replayServiceClient) Replay(ctx context.Context, in *ReplayRequest, opt
 	return out, nil
 }
 
+func (c *replayServiceClient) ListReplay(ctx context.Context, in *ListReplayRequest, opts ...grpc.CallOption) (*ListReplayResponse, error) {
+	out := new(ListReplayResponse)
+	err := c.cc.Invoke(ctx, "/gotocompany.optimus.core.v1beta1.ReplayService/ListReplay", in, out, opts...)
+	if err != nil {
+		return nil, err
+	}
+	return out, nil
+}
+
 // ReplayServiceServer is the server API for ReplayService service.
 // All implementations must embed UnimplementedReplayServiceServer
 // for forward compatibility
 type ReplayServiceServer interface {
 	Replay(context.Context, *ReplayRequest) (*ReplayResponse, error)
+	ListReplay(context.Context, *ListReplayRequest) (*ListReplayResponse, error)
 	mustEmbedUnimplementedReplayServiceServer()
 }
 
@@ -56,6 +67,9 @@ type UnimplementedReplayServiceServer struct {
 
 func (UnimplementedReplayServiceServer) Replay(context.Context, *ReplayRequest) (*ReplayResponse, error) {
 	return nil, status.Errorf(codes.Unimplemented, "method Replay not implemented")
+}
+func (UnimplementedReplayServiceServer) ListReplay(context.Context, *ListReplayRequest) (*ListReplayResponse, error) {
+	return nil, status.Errorf(codes.Unimplemented, "method ListReplay not implemented")
 }
 func (UnimplementedReplayServiceServer) mustEmbedUnimplementedReplayServiceServer() {}
 
@@ -88,6 +102,24 @@ func _ReplayService_Replay_Handler(srv interface{}, ctx context.Context, dec fun
 	return interceptor(ctx, in, info, handler)
 }
 
+func _ReplayService_ListReplay_Handler(srv interface{}, ctx context.Context, dec func(interface{}) error, interceptor grpc.UnaryServerInterceptor) (interface{}, error) {
+	in := new(ListReplayRequest)
+	if err := dec(in); err != nil {
+		return nil, err
+	}
+	if interceptor == nil {
+		return srv.(ReplayServiceServer).ListReplay(ctx, in)
+	}
+	info := &grpc.UnaryServerInfo{
+		Server:     srv,
+		FullMethod: "/gotocompany.optimus.core.v1beta1.ReplayService/ListReplay",
+	}
+	handler := func(ctx context.Context, req interface{}) (interface{}, error) {
+		return srv.(ReplayServiceServer).ListReplay(ctx, req.(*ListReplayRequest))
+	}
+	return interceptor(ctx, in, info, handler)
+}
+
 // ReplayService_ServiceDesc is the grpc.ServiceDesc for ReplayService service.
 // It's only intended for direct use with grpc.RegisterService,
 // and not to be introspected or modified (even as a copy)
@@ -98,6 +130,10 @@ var ReplayService_ServiceDesc = grpc.ServiceDesc{
 		{
 			MethodName: "Replay",
 			Handler:    _ReplayService_Replay_Handler,
+		},
+		{
+			MethodName: "ListReplay",
+			Handler:    _ReplayService_ListReplay_Handler,
 		},
 	},
 	Streams:  []grpc.StreamDesc{},

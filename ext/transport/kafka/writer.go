f0820d00a1d717fd99e5c0ce5791a1ccc7b39c63
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2023-03-31 14:49:49 +0530

    feat: publish events from optimus

---
+++ ext/transport/kafka/writer.go
@@ -0,0 +1,57 @@
+package kafka
+
+import (
+	"context"
+
+	"github.com/goto/salt/log"
+	"github.com/prometheus/client_golang/prometheus"
+	"github.com/prometheus/client_golang/prometheus/promauto"
+	"github.com/segmentio/kafka-go"
+)
+
+var (
+	kafkaQueueCounter = promauto.NewCounter(prometheus.CounterOpts{
+		Name: "kafka_publish_queue",
+		Help: "Events published to kafka topic",
+	})
+)
+
+type Message []byte
+
+type Writer struct {
+	kWriter *kafka.Writer
+}
+
+func NewWriter(kafkaBrokerUrls []string, topic string, logger log.Logger) *Writer {
+	writer := &kafka.Writer{
+		Addr:                   kafka.TCP(kafkaBrokerUrls...),
+		Topic:                  topic,
+		AllowAutoTopicCreation: true,
+		Balancer:               &kafka.LeastBytes{},
+		RequiredAcks:           kafka.RequireOne,
+		Logger:                 kafka.LoggerFunc(logger.Info),
+		ErrorLogger:            kafka.LoggerFunc(logger.Error),
+	}
+
+	return &Writer{kWriter: writer}
+}
+
+func (w *Writer) Close() {
+	_ = w.kWriter.Close()
+}
+
+func (w *Writer) Write(messages []Message) error {
+	kMessage := make([]kafka.Message, len(messages))
+	for i, m := range messages {
+		kMessage[i] = kafka.Message{
+			Value: m,
+		}
+	}
+
+	err := w.kWriter.WriteMessages(context.Background(), kMessage...)
+	if err == nil {
+		kafkaQueueCounter.Add(float64(len(messages)))
+		return nil
+	}
+	return err
+}

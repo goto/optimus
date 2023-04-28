8915e43a4296e918bd83a01fc78dd0aa03a860e1
Yash Bhardwaj <imyashbhardwaj@gmail.com>
2023-04-28 15:19:52 +0530

    feat: add scheduler events

---
+++ core/event/scheduler.go
@@ -0,0 +1,58 @@
+package event
+
+import (
+	"google.golang.org/protobuf/types/known/timestamppb"
+
+	"google.golang.org/protobuf/proto"
+
+	"github.com/goto/optimus/core/scheduler"
+	pbInt "github.com/goto/optimus/protos/gotocompany/optimus/integration/v1beta1"
+)
+
+type JobRunStateChanged struct {
+	Event
+
+	JobRun *scheduler.JobRun
+}
+
+func NewJobRunStateChangeEvent(jobRun *scheduler.JobRun) (*JobRunStateChanged, error) {
+	baseEvent, err := NewBaseEvent()
+	if err != nil {
+		return nil, err
+	}
+	return &JobRunStateChanged{
+		Event:  baseEvent,
+		JobRun: jobRun,
+	}, nil
+}
+
+func (j *JobRunStateChanged) Bytes() ([]byte, error) {
+	occurredAt := timestamppb.New(j.Event.OccurredAt)
+	var eventType pbInt.OptimusChangeEvent_EventType
+	switch j.JobRun.State {
+	case scheduler.StateWaitUpstream:
+		eventType = pbInt.OptimusChangeEvent_JOB_WAIT_UPSTREAM
+	case scheduler.StateInProgress:
+		eventType = pbInt.OptimusChangeEvent_JOB_IN_PROGRESS
+	case scheduler.StateSuccess:
+		eventType = pbInt.OptimusChangeEvent_JOB_SUCCESS
+	case scheduler.StateFailed:
+		eventType = pbInt.OptimusChangeEvent_JOB_FAILURE
+	}
+
+	optEvent := &pbInt.OptimusChangeEvent{
+		EventId:       j.Event.ID.String(),
+		OccurredAt:    occurredAt,
+		ProjectName:   j.JobRun.Tenant.ProjectName().String(),
+		NamespaceName: j.JobRun.Tenant.NamespaceName().String(),
+		EventType:     eventType,
+		Payload: &pbInt.OptimusChangeEvent_JobRun{
+			JobRun: &pbInt.JobRunPayload{
+				JobName:     j.JobRun.JobName.String(),
+				ScheduledAt: j.JobRun.ScheduledAt.String(),
+				JobRunId:    j.JobRun.ID.String(),
+			},
+		},
+	}
+	return proto.Marshal(optEvent)
+}

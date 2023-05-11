f39cfd15941d25bc3943f1e9ec8a3cba653bb4e7
Sandeep Bhardwaj <sandeep.bhardwaj@gojek.com>
2023-05-11 17:52:23 +0530

    refactor: trace the upload to scheduler

--- core/scheduler/service/deployment_service.go
+++ core/scheduler/service/deployment_service.go
@@ -4,26 +4,36 @@ import (
 	"context"
 	"fmt"
 
+	"go.opentelemetry.io/otel"
+
 	"github.com/goto/optimus/core/scheduler"
 	"github.com/goto/optimus/core/tenant"
 	"github.com/goto/optimus/internal/errors"
 )
 
-func (s JobRunService) UploadToScheduler(ctx context.Context, projectName tenant.ProjectName) error {
+func (s *JobRunService) UploadToScheduler(ctx context.Context, projectName tenant.ProjectName) error {
+	spanCtx, span := otel.Tracer("optimus").Start(ctx, "UploadToScheduler")
+	defer span.End()
+
 	multiError := errors.NewMultiError("errorInUploadToScheduler")
 	allJobsWithDetails, err := s.jobRepo.GetAll(ctx, projectName)
 	multiError.Append(err)
 	if allJobsWithDetails == nil {
 		return errors.MultiToError(multiError)
 	}
-	err = s.priorityResolver.Resolve(ctx, allJobsWithDetails)
+	span.AddEvent("got all the jobs to upload")
+
+	err = s.priorityResolver.Resolve(spanCtx, allJobsWithDetails)
 	if err != nil {
 		multiError.Append(err)
 		return errors.MultiToError(multiError)
 	}
+	span.AddEvent("done with priority resolution")
+
 	jobGroupByTenant := scheduler.GroupJobsByTenant(allJobsWithDetails)
 	for t, jobs := range jobGroupByTenant {
-		if err = s.deployJobsPerNamespace(ctx, t, jobs); err == nil {
+		span.AddEvent("uploading job specs")
+		if err = s.deployJobsPerNamespace(spanCtx, t, jobs); err == nil {
 			s.l.Debug(fmt.Sprintf("[success] namespace: %s, project: %s, deployed", t.NamespaceName().String(), t.ProjectName().String()))
 		}
 		multiError.Append(err)
@@ -31,7 +41,7 @@ func (s JobRunService) UploadToScheduler(ctx context.Context, projectName tenant
 	return multiError.ToErr()
 }
 
-func (s JobRunService) deployJobsPerNamespace(ctx context.Context, t tenant.Tenant, jobs []*scheduler.JobWithDetails) error {
+func (s *JobRunService) deployJobsPerNamespace(ctx context.Context, t tenant.Tenant, jobs []*scheduler.JobWithDetails) error {
 	err := s.scheduler.DeployJobs(ctx, t, jobs)
 	if err != nil {
 		return err
@@ -39,7 +49,7 @@ func (s JobRunService) deployJobsPerNamespace(ctx context.Context, t tenant.Tena
 	return s.cleanPerNamespace(ctx, t, jobs)
 }
 
-func (s JobRunService) cleanPerNamespace(ctx context.Context, t tenant.Tenant, jobs []*scheduler.JobWithDetails) error {
+func (s *JobRunService) cleanPerNamespace(ctx context.Context, t tenant.Tenant, jobs []*scheduler.JobWithDetails) error {
 	// get all stored job names
 	schedulerJobNames, err := s.scheduler.ListJobs(ctx, t)
 	if err != nil {

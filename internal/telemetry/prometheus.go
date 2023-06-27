53426a2a0cd35c4dd0042740ba9f114b40f33486
Arinda Arif <arindaarif05@gmail.com>
2023-06-27 16:11:03 +0700

    fix: concurrent read write issue in metric map

--- internal/telemetry/prometheus.go
+++ internal/telemetry/prometheus.go
@@ -2,14 +2,18 @@ package telemetry
 
 import (
 	"sort"
+	"sync"
 
 	"github.com/prometheus/client_golang/prometheus"
 	"github.com/prometheus/client_golang/prometheus/promauto"
 )
 
 var (
-	counterMetricMap = map[string]prometheus.Counter{}
+	counterMetricMap   = map[string]prometheus.Counter{}
+	counterMetricMutex = sync.Mutex{}
+
 	gaugeMetricMap   = map[string]prometheus.Gauge{}
+	gaugeMetricMutex = sync.Mutex{}
 )
 
 func getKey(metric string, labels map[string]string) string {
@@ -27,6 +31,10 @@ func getKey(metric string, labels map[string]string) string {
 
 func NewCounter(metric string, labels map[string]string) prometheus.Counter {
 	metricKey := getKey(metric, labels)
+
+	counterMetricMutex.Lock()
+	defer counterMetricMutex.Unlock()
+
 	if _, ok := counterMetricMap[metricKey]; !ok {
 		counterMetricMap[metricKey] = promauto.NewCounter(prometheus.CounterOpts{Name: metric, ConstLabels: labels})
 	}
@@ -35,6 +43,10 @@ func NewCounter(metric string, labels map[string]string) prometheus.Counter {
 
 func NewGauge(metric string, labels map[string]string) prometheus.Gauge {
 	metricKey := getKey(metric, labels)
+
+	gaugeMetricMutex.Lock()
+	defer gaugeMetricMutex.Unlock()
+
 	if _, ok := gaugeMetricMap[metricKey]; !ok {
 		gaugeMetricMap[metricKey] = promauto.NewGauge(prometheus.GaugeOpts{Name: metric, ConstLabels: labels})
 	}

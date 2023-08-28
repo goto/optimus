737207a9a5b63c33776401021cca464bb966d39e
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2023-08-28 10:26:05 +0700

    fix: change preset from entity and add more tests

--- core/tenant/preset.go
+++ core/tenant/preset.go
@@ -7,8 +7,6 @@ import (
 	"github.com/goto/optimus/internal/models"
 )
 
-const EntityPreset = "preset"
-
 type Preset struct {
 	name        string
 	description string
@@ -28,19 +26,47 @@ func (p Preset) Window() models.Window {
 	return p.window
 }
 
+func (p Preset) Equal(incoming Preset) bool {
+	if p.name != incoming.name {
+		return false
+	}
+
+	if p.description != incoming.description {
+		return false
+	}
+
+	if p.window.GetTruncateTo() != incoming.window.GetTruncateTo() {
+		return false
+	}
+
+	if p.window.GetOffset() != incoming.window.GetOffset() {
+		return false
+	}
+
+	if p.window.GetSize() != incoming.window.GetSize() {
+		return false
+	}
+
+	return true
+}
+
 func NewPreset(name, description, truncateTo, offset, size string) (Preset, error) {
 	cleanedName := strings.ToLower(strings.TrimSpace(name))
 	if cleanedName == "" {
-		return Preset{}, errors.InvalidArgument(EntityPreset, "cleaned preset name is empty")
+		return Preset{}, errors.InvalidArgument(EntityProject, "cleaned preset name is empty")
 	}
 
 	cleanedDescription := strings.TrimSpace(description)
 	if cleanedDescription == "" {
-		return Preset{}, errors.InvalidArgument(EntityPreset, "cleaned preset description is empty")
+		return Preset{}, errors.InvalidArgument(EntityProject, "cleaned preset description is empty")
 	}
 
 	window, err := models.NewWindow(2, truncateTo, offset, size)
 	if err != nil {
+		return Preset{}, err
+	}
+
+	if err := window.Validate(); err != nil {
 		return Preset{}, err
 	}
 
--- core/tenant/preset_test.go
+++ core/tenant/preset_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-func TestEntityPreset(t *testing.T) {
+func TestPreset(t *testing.T) {
 	t.Run("NewPreset", func(t *testing.T) {
 		t.Run("should return zero preset and error if name is invalid", func(t *testing.T) {
 			type presetInput struct {
@@ -32,7 +32,7 @@ func TestEntityPreset(t *testing.T) {
 						offset:      "-1h",
 						size:        "24h",
 					},
-					expectedErrorMessage: "invalid argument for entity preset: cleaned preset name is empty",
+					expectedErrorMessage: "invalid argument for entity project: cleaned preset name is empty",
 				},
 				{
 					caseName: "cleaned name resulted in empty",
@@ -43,7 +43,7 @@ func TestEntityPreset(t *testing.T) {
 						offset:      "-1h",
 						size:        "24h",
 					},
-					expectedErrorMessage: "invalid argument for entity preset: cleaned preset name is empty",
+					expectedErrorMessage: "invalid argument for entity project: cleaned preset name is empty",
 				},
 				{
 					caseName: "description is empty",
@@ -54,7 +54,7 @@ func TestEntityPreset(t *testing.T) {
 						offset:      "-1h",
 						size:        "24h",
 					},
-					expectedErrorMessage: "invalid argument for entity preset: cleaned preset description is empty",
+					expectedErrorMessage: "invalid argument for entity project: cleaned preset description is empty",
 				},
 				{
 					caseName: "cleaned description resulted in empty",
@@ -65,7 +65,18 @@ func TestEntityPreset(t *testing.T) {
 						offset:      "-1h",
 						size:        "24h",
 					},
-					expectedErrorMessage: "invalid argument for entity preset: cleaned preset description is empty",
+					expectedErrorMessage: "invalid argument for entity project: cleaned preset description is empty",
+				},
+				{
+					caseName: "window is invalid",
+					presetInput: presetInput{
+						name:        "yesterday",
+						description: "preset for testing",
+						truncateTo:  "Z",
+						offset:      "-1h",
+						size:        "24h",
+					},
+					expectedErrorMessage: "error validating truncate_to: invalid option provided, provide one of: [h d w M]",
 				},
 			}
 
@@ -174,6 +185,114 @@ func TestEntityPreset(t *testing.T) {
 				actualWindow := preset.Window()
 
 				assert.EqualValues(t, expectedWindow, actualWindow)
+			})
+		})
+
+		t.Run("Equal", func(t *testing.T) {
+			t.Run("should return false if not equal", func(t *testing.T) {
+				name := "yesterday"
+				description := "preset for testing"
+				truncateTo := "d"
+				offset := "-1h"
+				size := "24h"
+
+				presetReference, err := tenant.NewPreset(name, description, truncateTo, offset, size)
+				assert.NotZero(t, presetReference)
+				assert.NoError(t, err)
+
+				type presetInput struct {
+					name        string
+					description string
+					truncateTo  string
+					offset      string
+					size        string
+				}
+				testCases := []struct {
+					caseName    string
+					presetInput presetInput
+				}{
+					{
+						caseName: "different name",
+						presetInput: presetInput{
+							name:        "different_name",
+							description: description,
+							truncateTo:  truncateTo,
+							offset:      offset,
+							size:        size,
+						},
+					},
+					{
+						caseName: "different description",
+						presetInput: presetInput{
+							name:        name,
+							description: "different description for test",
+							truncateTo:  truncateTo,
+							offset:      offset,
+							size:        size,
+						},
+					},
+					{
+						caseName: "different truncate_to",
+						presetInput: presetInput{
+							name:        name,
+							description: description,
+							truncateTo:  "M",
+							offset:      offset,
+							size:        size,
+						},
+					},
+					{
+						caseName: "different offset",
+						presetInput: presetInput{
+							name:        name,
+							description: description,
+							truncateTo:  truncateTo,
+							offset:      "-2h",
+							size:        size,
+						},
+					},
+					{
+						caseName: "different name",
+						presetInput: presetInput{
+							name:        name,
+							description: description,
+							truncateTo:  truncateTo,
+							offset:      offset,
+							size:        "23h",
+						},
+					},
+				}
+
+				for _, test := range testCases {
+					name := test.presetInput.name
+					description := test.presetInput.description
+					truncateTo := test.presetInput.truncateTo
+					offset := test.presetInput.offset
+					size := test.presetInput.size
+
+					actualPreset, actualError := tenant.NewPreset(name, description, truncateTo, offset, size)
+					assert.NotZero(t, actualPreset, test.caseName)
+					assert.NoError(t, actualError, test.caseName)
+					assert.False(t, presetReference.Equal(actualPreset))
+				}
+			})
+
+			t.Run("should return true if equal", func(t *testing.T) {
+				name := "yesterday"
+				description := "preset for testing"
+				truncateTo := "d"
+				offset := "-1h"
+				size := "24h"
+
+				presetReference, err := tenant.NewPreset(name, description, truncateTo, offset, size)
+				assert.NotZero(t, presetReference)
+				assert.NoError(t, err)
+
+				actualPreset, err := tenant.NewPreset(name, description, truncateTo, offset, size)
+				assert.NotZero(t, actualPreset)
+				assert.NoError(t, err)
+
+				assert.True(t, presetReference.Equal(actualPreset))
 			})
 		})
 	})

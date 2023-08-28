0456f8340598a1eebebfead9edba0f5316d66af3
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2023-08-28 10:30:33 +0700

    feat: add migration for presets

---
+++ internal/store/postgres/migrations/000057_create_preset_table.up.sql
@@ -0,0 +1,20 @@
+CREATE TABLE IF NOT EXISTS preset (
+    id UUID PRIMARY KEY DEFAULT uuid_generate_v4(),
+
+    project_name    VARCHAR(100) NOT NULL,
+    name            VARCHAR(100) NOT NULL,
+    description     TEXT NOT NULL,
+
+    truncate_to     VARCHAR(10),
+    offset          VARCHAR(10),
+    size            VARCHAR(10),
+
+    created_at TIMESTAMP WITH TIME ZONE NOT NULL,
+    updated_at TIMESTAMP WITH TIME ZONE NOT NULL,
+
+    UNIQUE (project_name, name)
+);
+
+CREATE INDEX IF NOT EXISTS preset_id_idx ON preset USING btree (id);
+CREATE INDEX IF NOT EXISTS preset_project_name_idx ON preset USING btree (project_name);
+CREATE INDEX IF NOT EXISTS preset_project_name_name_idx ON preset USING btree (project_name, name);

feabe742a0dd1487296967e3e84d52bef352ee20
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2023-09-01 16:25:33 +0700

    test: add unit test for window

--- internal/lib/window/window_test.go
+++ internal/lib/window/window_test.go
@@ -29,6 +29,7 @@ func TestWindow(t *testing.T) {
 			assert.Equal(t, "2023-10-01T00:00:00Z", interval.End.Format(time.RFC3339))
 		})
 	})
+
 	t.Run("FromBaseWindow", func(t *testing.T) {
 		t.Run("returns error when unable to get end of window", func(t *testing.T) {
 			w1, _ := models.NewWindow(2, "Z", "0", "24h")
@@ -57,6 +58,63 @@ func TestWindow(t *testing.T) {
 			assert.NoError(t, err)
 			assert.Equal(t, "2023-08-31T00:00:00Z", interval.Start.Format(time.RFC3339))
 			assert.Equal(t, "2023-09-01T00:00:00Z", interval.End.Format(time.RFC3339))
+		})
+		t.Run("should return zero and error if error parsing schedule", func(t *testing.T) {
+			schedule := "-1 * * * *"
+
+			actualWindow, actualError := window.FromSchedule(schedule)
+
+			assert.Zero(t, actualWindow)
+			assert.Error(t, actualError)
+			assert.ErrorContains(t, actualError, "unable to parse job cron interval")
+		})
+		t.Run("should return window and nil if no error is encountered", func(t *testing.T) {
+			schedule := "0 * * * *"
+
+			actualWindow, actualError := window.FromSchedule(schedule)
+
+			assert.NotZero(t, actualWindow)
+			assert.NoError(t, actualError)
+		})
+	})
+
+	t.Run("GetInterval", func(t *testing.T) {
+		const (
+			version    = 2
+			truncateTo = "d"
+			offset     = "-24h"
+			size       = "24h"
+		)
+
+		referenceTime := time.Now()
+
+		t.Run("should return zero and error if error when getting start time", func(t *testing.T) {
+			invalidSize := "24"
+
+			baseWindow, err := models.NewWindow(version, truncateTo, offset, invalidSize)
+			assert.NotNil(t, baseWindow)
+			assert.NoError(t, err)
+
+			w := window.FromBaseWindow(baseWindow)
+
+			actualInterval, actualError := w.GetInterval(referenceTime)
+
+			assert.Zero(t, actualInterval)
+			assert.Error(t, actualError)
+			assert.ErrorContains(t, actualError, "missing unit in duration")
+		})
+
+		t.Run("should return interval and nil if no error is encountered", func(t *testing.T) {
+			baseWindow, err := models.NewWindow(version, truncateTo, offset, size)
+			assert.NotNil(t, baseWindow)
+			assert.NoError(t, err)
+
+			w := window.FromBaseWindow(baseWindow)
+
+			actualInterval, actualError := w.GetInterval(referenceTime)
+
+			assert.NotZero(t, actualInterval)
+			assert.NoError(t, actualError)
 		})
 	})
 }

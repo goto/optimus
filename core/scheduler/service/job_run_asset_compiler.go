c8581205d8c697cb55cf5c7c20b99adc6421d2d9
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2023-09-06 14:00:43 +0700

    feat: introduce compile assets as part of binary deprecation (#133)
    * refactor: move compile asset to internal/engine
    
    * refactor: compile bq2bq query functionality
    
    * remove: compile asset before generate upstreams
    
    * refactor: generate dependencies to only use required params
    
    * refactor: move compile query to scheduler specific domain
    
    * remove: unecessary interface

--- core/scheduler/service/job_run_asset_compiler.go
+++ core/scheduler/service/job_run_asset_compiler.go
@@ -3,11 +3,11 @@ package service
 import (
 	"context"
 	"fmt"
+	"strings"
 	"time"
 
 	"github.com/goto/salt/log"
 
-	"github.com/goto/optimus/core/job/service/bq2bq"
 	"github.com/goto/optimus/core/scheduler"
 	"github.com/goto/optimus/sdk/plugin"
 )
@@ -52,15 +52,17 @@ func (c *JobRunAssetsCompiler) CompileJobRunAssets(ctx context.Context, job *sch
 	}
 
 	inputFiles := job.Assets
-
-	if job.Task.Name == "bq2bq" { // compile assets exclusive only for bq2bq plugin
+	method, ok1 := job.Task.Config["LOAD_METHOD"]
+	query, ok2 := job.Assets["query.sql"]
+	// compile assets exclusive only for bq2bq plugin with replace load method and contains query.sql in asset
+	if ok1 && ok2 && method == "REPLACE" && job.Task.Name == "bq2bq" {
 		// check if task needs to override the compilation behaviour
-		compiledAssets, err := bq2bq.CompileAssets(ctx, c.compiler, startTime, endTime, job.Task.Config, job.Assets, systemEnvVars)
+		compiledQuery, err := c.CompileQuery(ctx, startTime, endTime, query, systemEnvVars)
 		if err != nil {
-			c.logger.Error("error compiling assets through plugin dependency mod: %s", err)
+			c.logger.Error("error compiling assets: %s", err.Error())
 			return nil, err
 		}
-		inputFiles = compiledAssets
+		inputFiles["query.sql"] = compiledQuery
 	}
 
 	fileMap, err := c.compiler.Compile(inputFiles, contextForTask)
@@ -69,6 +71,55 @@ func (c *JobRunAssetsCompiler) CompileJobRunAssets(ctx context.Context, job *sch
 		return nil, err
 	}
 	return fileMap, nil
+}
+
+func (c *JobRunAssetsCompiler) CompileQuery(ctx context.Context, startTime, endTime time.Time, query string, envs map[string]string) (string, error) {
+	// partition window in range
+	instanceEnvMap := map[string]interface{}{}
+	for name, value := range envs {
+		instanceEnvMap[name] = value
+	}
+
+	// TODO: making few assumptions here, should be documented
+	// assume destination table is time partitioned
+	// assume table is partitioned as DAY
+	const dayHours = time.Duration(24)
+	partitionDelta := time.Hour * dayHours
+
+	// find destination partitions
+	var destinationsPartitions []struct {
+		start time.Time
+		end   time.Time
+	}
+	for currentPart := startTime; currentPart.Before(endTime); currentPart = currentPart.Add(partitionDelta) {
+		destinationsPartitions = append(destinationsPartitions, struct {
+			start time.Time
+			end   time.Time
+		}{
+			start: currentPart,
+			end:   currentPart.Add(partitionDelta),
+		})
+	}
+
+	// check if window size is greater than partition delta(a DAY), if not do nothing
+	if endTime.Sub(startTime) <= partitionDelta {
+		return "", nil
+	}
+
+	var parsedQueries []string
+	queryMap := map[string]string{"query": query}
+	for _, part := range destinationsPartitions {
+		instanceEnvMap["DSTART"] = part.start.Format(time.RFC3339)
+		instanceEnvMap["DEND"] = part.end.Format(time.RFC3339)
+		compiledQueryMap, err := c.compiler.Compile(queryMap, instanceEnvMap)
+		if err != nil {
+			return "", err
+		}
+		parsedQueries = append(parsedQueries, compiledQueryMap["query"])
+	}
+
+	queryFileReplaceBreakMarker := "\n--*--optimus-break-marker--*--\n"
+	return strings.Join(parsedQueries, queryFileReplaceBreakMarker), nil
 }
 
 // TODO: deprecate after changing type for plugin

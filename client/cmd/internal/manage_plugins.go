f20d8d20208150b0860bb76cf6c8af852462e85c
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2023-09-14 05:51:46 +0700

    fix: logger on manage plugin

--- client/cmd/internal/manage_plugins.go
+++ client/cmd/internal/manage_plugins.go
@@ -13,6 +13,9 @@ import (
 
 // InitPlugins triggers initialization of all available plugins
 func InitPlugins(logLevel config.LogLevel) (*models.PluginRepository, error) {
+	if logLevel != config.LogLevelDebug {
+		logLevel = config.LogLevelInfo
+	}
 	logger := log.NewLogrus(
 		log.LogrusWithLevel(logLevel.String()),
 		log.LogrusWithWriter(os.Stdout),

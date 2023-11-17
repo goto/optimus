4a8e471016bf4680f28679be6f2bc34674dd350d
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2023-11-17 14:08:23 +0700

    feat: add label spec validation (#181)
    * feat: add label under internal
    
    * feat: add labels validation and its unit test
    
    * feat: add validation for metadata
    
    * refactor: resource metadata validation for more readability
    
    * feat: add label validation for job spec
    
    * refactor: rename label to labels
    
    * chore: update error message for better clarity
    
    * refactor: separate key and value validation
    
    * fix: change labels to not return error

---
+++ internal/lib/labels/labels_test.go
@@ -0,0 +1,202 @@
+package labels_test
+
+import (
+	"fmt"
+	"strings"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/goto/optimus/internal/lib/labels"
+)
+
+func TestFromMap(t *testing.T) {
+	t.Run("should return empty if incoming labels are nil", func(t *testing.T) {
+		var incoming map[string]string
+
+		actualLabels := labels.FromMap(incoming)
+
+		assert.NotNil(t, actualLabels)
+		assert.Empty(t, actualLabels)
+	})
+
+	t.Run("should return labels and nil if no error is encountered", func(t *testing.T) {
+		incoming := map[string]string{
+			"key1": "value1",
+		}
+
+		actualLabels := labels.FromMap(incoming)
+
+		assert.NotEmpty(t, actualLabels)
+	})
+}
+
+func TestLabels(t *testing.T) {
+	t.Run("should return error if labels is nil", func(t *testing.T) {
+		var incoming labels.Labels
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "labels is nil")
+	})
+
+	t.Run("should return error if labels is more than 32 in length", func(t *testing.T) {
+		l := make(map[string]string)
+		for i := 0; i < 33; i++ {
+			key := fmt.Sprintf("key-%d", i)
+			value := fmt.Sprintf("value-%d", i)
+			l[key] = value
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "labels length is more than [32]")
+	})
+
+	t.Run("should return error if labels key is empty", func(t *testing.T) {
+		l := map[string]string{
+			"": "test_value",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "key is empty")
+	})
+
+	t.Run("should return error if labels key length is more than 63", func(t *testing.T) {
+		l := map[string]string{
+			strings.Repeat("a", 64): "test_value",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "key length is more than [63]")
+	})
+
+	t.Run("should return error if labels key contains characters outside alphanumerics, underscores, and dashes", func(t *testing.T) {
+		l := map[string]string{
+			"invalid_key_with_!": "test_value",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "key should only be combination of lower case letters, numerics, underscores, and/or dashes")
+	})
+
+	t.Run("should return error if labels key contains upper case letters", func(t *testing.T) {
+		l := map[string]string{
+			"invalid_key_with_A": "test_value",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "key should only be combination of lower case letters, numerics, underscores, and/or dashes")
+	})
+
+	t.Run("should return error if labels key does not start with alphanumeric", func(t *testing.T) {
+		l := map[string]string{
+			"-invalid_key": "test_value",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "key should start and end with alphanumerics only")
+	})
+
+	t.Run("should return error if labels key does not end with alphanumeric", func(t *testing.T) {
+		l := map[string]string{
+			"invalid_key-": "test_value",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "key should start and end with alphanumerics only")
+	})
+
+	t.Run("should return error if labels value is empty", func(t *testing.T) {
+		l := map[string]string{
+			"test_key": "",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "value is empty")
+	})
+
+	t.Run("should return error if labels value length is more than 63", func(t *testing.T) {
+		l := map[string]string{
+			"test_key": strings.Repeat("a", 64),
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "value length is more than [63]")
+	})
+
+	t.Run("should return error if labels value contains characters outside alphanumerics, underscores, and dashes", func(t *testing.T) {
+		l := map[string]string{
+			"test_key": "invalid_value_with_!",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "value should only be combination of lower case letters, numerics, underscores, and/or dashes")
+	})
+
+	t.Run("should return error if labels value contains upper case letters", func(t *testing.T) {
+		l := map[string]string{
+			"test_key": "invalid_value_with_A",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "value should only be combination of lower case letters, numerics, underscores, and/or dashes")
+	})
+
+	t.Run("should return error if labels value does not start with alphanumeric", func(t *testing.T) {
+		l := map[string]string{
+			"test_key": "-invalid_value",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "value should start and end with alphanumerics only")
+	})
+
+	t.Run("should return error if labels value does not end with alphanumeric", func(t *testing.T) {
+		l := map[string]string{
+			"test_key": "invalid_value-",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.ErrorContains(t, actualError, "value should start and end with alphanumerics only")
+	})
+
+	t.Run("should return nil if 'labels' follow valid spec", func(t *testing.T) {
+		l := map[string]string{
+			"test_key":      "test_value",
+			"id":            "abcd-efgh-ijkl-mnop",
+			"resource-name": "1_resource_test",
+			"job-name":      "job_test_1",
+		}
+		incoming := labels.FromMap(l)
+
+		actualError := incoming.Validate()
+
+		assert.NoError(t, actualError)
+	})
+}

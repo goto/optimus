08b40b254e9372ed50a2f697824b72b015b6b422
Anwar Hidayat <15167551+irainia@users.noreply.github.com>
2024-03-07 14:12:54 +0700

    refactor!: move v1 and v2 window playground to a temporary v1v2 package

---
+++ client/cmd/playground/window/v1v2/doc.go
@@ -0,0 +1,4 @@
+// v1v2 package is a temporary playground package for window v1 and v2.
+// Along with deprecation of window v1 and v2, this package will also be removed.
+// This makes playground to be only for window v3.
+package v1v2

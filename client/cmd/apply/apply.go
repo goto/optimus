1780eca904570686c4301f4a20ef59277c58f045
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2024-05-08 18:31:28 +0700

    feat: execute apply on job and resource operations

--- client/cmd/apply/apply.go
+++ client/cmd/apply/apply.go
@@ -1,25 +1,44 @@
 package apply
 
 import (
+	"context"
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/gocarina/gocsv"
+	"github.com/spf13/afero"
+	"github.com/spf13/cobra"
+
+	"github.com/goto/salt/log"
+
 	"github.com/goto/optimus/client/cmd/internal"
 	"github.com/goto/optimus/client/cmd/internal/connection"
 	"github.com/goto/optimus/client/cmd/internal/logger"
+	"github.com/goto/optimus/client/cmd/internal/plan"
+	"github.com/goto/optimus/client/local"
+	"github.com/goto/optimus/client/local/model"
+	"github.com/goto/optimus/client/local/specio"
 	"github.com/goto/optimus/config"
-	"github.com/goto/salt/log"
-	"github.com/spf13/cobra"
+	pb "github.com/goto/optimus/protos/gotocompany/optimus/core/v1beta1"
+)
+
+const (
+	applyTimeout     = time.Minute * 5
+	executedPlanFile = "plan_apply.csv"
 )
 
 type applyCommand struct {
-	logger     log.Logger
-	connection connection.Connection
+	logger                 log.Logger
+	connection             connection.Connection
+	config                 *config.ClientConfig
+	jobSpecReadWriter      local.SpecReadWriter[*model.JobSpec]
+	resourceSpecReadWriter local.SpecReadWriter[*model.ResourceSpec]
 
+	verbose        bool
 	configFilePath string
-
-	sources []string
-
-	projectName string
-	host        string
-	namespace   string
+	sources        []string
+	output         string
 }
 
 // NewApplyCommand apply the job / resource changes
@@ -42,9 +61,8 @@ func (c *applyCommand) injectFlags(cmd *cobra.Command) {
 	cmd.PersistentFlags().StringVarP(&c.configFilePath, "config", "c", config.EmptyPath, "File path for client configuration")
 
 	cmd.Flags().StringSliceVarP(&c.sources, "sources", "s", c.sources, "Sources of plan result to be executed")
-
-	// Mandatory flags if config is not set
-	cmd.Flags().StringVar(&c.host, "host", "", "Optimus service endpoint url")
+	cmd.Flags().StringVarP(&c.output, "output", "o", executedPlanFile, "Output of plan result after executed")
+	cmd.Flags().BoolVarP(&c.verbose, "verbose", "v", false, "Determines whether to show the complete message or just the summary")
 }
 
 func (c *applyCommand) PreRunE(cmd *cobra.Command, _ []string) error {
@@ -53,20 +71,320 @@ func (c *applyCommand) PreRunE(cmd *cobra.Command, _ []string) error {
 	if err != nil {
 		return err
 	}
-	defer func() {
-		c.logger = logger.NewClientLogger()
-		c.connection = connection.New(c.logger, conf)
-	}()
-
 	if conf == nil {
-		internal.MarkFlagsRequired(cmd, []string{"host"})
-		return nil
+		return fmt.Errorf("config should be specified")
 	}
-
-	c.host = conf.Host
+	jobSpecReadWriter, err := specio.NewJobSpecReadWriter(afero.NewOsFs(), specio.WithJobSpecParentReading())
+	if err != nil {
+		return fmt.Errorf("couldn't instantiate job spec reader")
+	}
+	resourceSpecReadWriter, err := specio.NewResourceSpecReadWriter(afero.NewOsFs())
+	if err != nil {
+		return fmt.Errorf("couldn't instantiate resource spec reader")
+	}
+	c.logger = logger.NewClientLogger()
+	c.connection = connection.New(c.logger, conf)
+	c.jobSpecReadWriter = jobSpecReadWriter
+	c.resourceSpecReadWriter = resourceSpecReadWriter
+	c.config = conf
 	return nil
 }
 
-func (c *applyCommand) RunE(_ *cobra.Command, _ []string) error {
-	return nil
+func (c *applyCommand) RunE(cmd *cobra.Command, _ []string) error {
+	conn, err := c.connection.Create(c.config.Host)
+	if err != nil {
+		return err
+	}
+	defer conn.Close()
+
+	jobClient := pb.NewJobSpecificationServiceClient(conn)
+	resourceClient := pb.NewResourceServiceClient(conn)
+	ctx, dialCancel := context.WithTimeout(cmd.Context(), applyTimeout)
+	defer dialCancel()
+
+	// read from plan file
+	basePlans, err := c.getPlans()
+	if err != nil {
+		return err
+	}
+	plans := basePlans.GetByProjectName(c.config.Project.Name)
+
+	// prepare for proto request
+	var (
+		addJobRequest         = []*pb.AddJobSpecificationsRequest{}
+		updateJobRequest      = []*pb.UpdateJobSpecificationsRequest{}
+		deleteJobRequest      = []*pb.DeleteJobSpecificationRequest{}
+		addResourceRequest    = []*pb.CreateResourceRequest{}
+		updateResourceRequest = []*pb.UpdateResourceRequest{}
+		deleteResourceRequest = []*pb.DeleteResourceRequest{}
+	)
+
+	for _, namespace := range c.config.Namespaces {
+		addJobRequest = append(addJobRequest, c.getAddJobRequest(namespace, plans)...)
+		updateJobRequest = append(updateJobRequest, c.getUpdateJobRequest(namespace, plans)...)
+		deleteJobRequest = append(deleteJobRequest, c.getDeleteJobRequest(c.config.Project.Name, namespace, plans)...)
+		addResourceRequest = append(addResourceRequest, c.getAddResourceRequest(c.config.Project.Name, namespace, plans)...)
+		updateResourceRequest = append(updateResourceRequest, c.getUpdateResourceRequest(c.config.Project.Name, namespace, plans)...)
+		deleteResourceRequest = append(deleteResourceRequest, c.getDeleteResourceRequest(c.config.Project.Name, namespace, plans)...)
+	}
+
+	// send to server based on operation
+	deletedJobs := c.executeJobDelete(ctx, jobClient, deleteJobRequest)
+	addedJobs := c.executeJobAdd(ctx, jobClient, addJobRequest)
+	updatedJobs := c.executeJobUpdate(ctx, jobClient, updateJobRequest)
+	deletedResources := c.executeResourceDelete(ctx, resourceClient, deleteResourceRequest)
+	addedResources := c.executeResourceAdd(ctx, resourceClient, addResourceRequest)
+	updatedResources := c.executeResourceUpdate(ctx, resourceClient, updateResourceRequest)
+
+	// update plan file
+	isExecuted := true
+	basePlans.UpdateExecutedByNames(isExecuted, plan.KindJob, deletedJobs...)
+	basePlans.UpdateExecutedByNames(isExecuted, plan.KindJob, addedJobs...)
+	basePlans.UpdateExecutedByNames(isExecuted, plan.KindJob, updatedJobs...)
+	basePlans.UpdateExecutedByNames(isExecuted, plan.KindResource, deletedResources...)
+	basePlans.UpdateExecutedByNames(isExecuted, plan.KindResource, addedResources...)
+	basePlans.UpdateExecutedByNames(isExecuted, plan.KindResource, updatedResources...)
+
+	return c.savePlans(basePlans)
+}
+
+func (c *applyCommand) executeJobDelete(ctx context.Context, client pb.JobSpecificationServiceClient, requests []*pb.DeleteJobSpecificationRequest) []string {
+	deletedJobs := []string{}
+	for _, request := range requests {
+		response, err := client.DeleteJobSpecification(ctx, request)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		if response.Success {
+			deletedJobs = append(deletedJobs, request.JobName)
+		}
+	}
+	return deletedJobs
+}
+
+func (c *applyCommand) executeJobAdd(ctx context.Context, client pb.JobSpecificationServiceClient, requests []*pb.AddJobSpecificationsRequest) []string {
+	addedJobs := []string{}
+	for _, request := range requests {
+		response, err := client.AddJobSpecifications(ctx, request)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		addedJobs = append(addedJobs, response.JobNameSuccesses...)
+	}
+	return addedJobs
+}
+
+func (c *applyCommand) executeJobUpdate(ctx context.Context, client pb.JobSpecificationServiceClient, requests []*pb.UpdateJobSpecificationsRequest) []string {
+	updatedJobs := []string{}
+	for _, request := range requests {
+		response, err := client.UpdateJobSpecifications(ctx, request)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		updatedJobs = append(updatedJobs, response.JobNameSuccesses...)
+	}
+	return updatedJobs
+}
+
+func (c *applyCommand) executeResourceDelete(ctx context.Context, client pb.ResourceServiceClient, requests []*pb.DeleteResourceRequest) []string {
+	deletedResources := []string{}
+	for _, request := range requests {
+		_, err := client.DeleteResource(ctx, request)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		deletedResources = append(deletedResources, request.ResourceName)
+	}
+	return deletedResources
+}
+
+func (c *applyCommand) executeResourceAdd(ctx context.Context, client pb.ResourceServiceClient, requests []*pb.CreateResourceRequest) []string {
+	addedResources := []string{}
+	for _, request := range requests {
+		response, err := client.CreateResource(ctx, request)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		if response.Success {
+			addedResources = append(addedResources, request.Resource.Name)
+		}
+	}
+	return addedResources
+}
+
+func (c *applyCommand) executeResourceUpdate(ctx context.Context, client pb.ResourceServiceClient, requests []*pb.UpdateResourceRequest) []string {
+	updatedResources := []string{}
+	for _, request := range requests {
+		response, err := client.UpdateResource(ctx, request)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		if response.Success {
+			updatedResources = append(updatedResources, request.Resource.Name)
+		}
+	}
+	return updatedResources
+}
+
+func (c *applyCommand) getAddJobRequest(namespace *config.Namespace, plans plan.Plans) []*pb.AddJobSpecificationsRequest {
+	jobsToBeSend := []*pb.JobSpecification{}
+	for _, plan := range plans.GetByNamespaceName(namespace.Name).GetByKind(plan.KindJob).GetByOperation(plan.OperationCreate) {
+		jobSpec, err := c.jobSpecReadWriter.ReadByName(".", plan.KindName)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		jobsToBeSend = append(jobsToBeSend, jobSpec.ToProto())
+	}
+
+	return []*pb.AddJobSpecificationsRequest{
+		&pb.AddJobSpecificationsRequest{
+			ProjectName:   c.config.Project.Name,
+			NamespaceName: namespace.Name,
+			Specs:         jobsToBeSend,
+		},
+	}
+}
+
+func (c *applyCommand) getUpdateJobRequest(namespace *config.Namespace, plans plan.Plans) []*pb.UpdateJobSpecificationsRequest {
+	jobsToBeSend := []*pb.JobSpecification{}
+	for _, plan := range plans.GetByNamespaceName(namespace.Name).GetByKind(plan.KindJob).GetByOperation(plan.OperationUpdate) {
+		jobSpec, err := c.jobSpecReadWriter.ReadByName(".", plan.KindName)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		jobsToBeSend = append(jobsToBeSend, jobSpec.ToProto())
+	}
+
+	return []*pb.UpdateJobSpecificationsRequest{
+		&pb.UpdateJobSpecificationsRequest{
+			ProjectName:   c.config.Project.Name,
+			NamespaceName: namespace.Name,
+			Specs:         jobsToBeSend,
+		},
+	}
+}
+
+func (c *applyCommand) getDeleteJobRequest(projectName string, namespace *config.Namespace, plans plan.Plans) []*pb.DeleteJobSpecificationRequest {
+	jobsToBeDeleted := []*pb.DeleteJobSpecificationRequest{}
+	for _, plan := range plans.GetByNamespaceName(namespace.Name).GetByKind(plan.KindJob).GetByOperation(plan.OperationDelete) {
+		jobSpec, err := c.jobSpecReadWriter.ReadByName(".", plan.KindName)
+		if err != nil {
+			c.logger.Error(err.Error())
+			continue
+		}
+		jobsToBeDeleted = append(jobsToBeDeleted, &pb.DeleteJobSpecificationRequest{
+			ProjectName:   projectName,
+			NamespaceName: namespace.Name,
+			JobName:       jobSpec.Name,
+			CleanHistory:  false,
+			Force:         false,
+		})
+	}
+	return jobsToBeDeleted
+}
+
+func (c *applyCommand) getAddResourceRequest(projectName string, namespace *config.Namespace, plans plan.Plans) []*pb.CreateResourceRequest {
+	resourcesToBeCreate := []*pb.CreateResourceRequest{}
+	for _, plan := range plans.GetByNamespaceName(namespace.Name).GetByKind(plan.KindResource).GetByOperation(plan.OperationCreate) {
+		for _, ds := range namespace.Datastore {
+			resourceSpec, err := c.resourceSpecReadWriter.ReadByName(".", plan.KindName)
+			if err != nil {
+				c.logger.Error(err.Error())
+				continue
+			}
+			resourceSpecProto, err := resourceSpec.ToProto()
+			if err != nil {
+				c.logger.Error(err.Error())
+				continue
+			}
+			resourcesToBeCreate = append(resourcesToBeCreate, &pb.CreateResourceRequest{
+				ProjectName:   projectName,
+				NamespaceName: namespace.Name,
+				DatastoreName: ds.Type,
+				Resource:      resourceSpecProto,
+			})
+		}
+	}
+	return resourcesToBeCreate
+}
+
+func (c *applyCommand) getUpdateResourceRequest(projectName string, namespace *config.Namespace, plans plan.Plans) []*pb.UpdateResourceRequest {
+	resourcesToBeUpdate := []*pb.UpdateResourceRequest{}
+	for _, plan := range plans.GetByNamespaceName(namespace.Name).GetByKind(plan.KindResource).GetByOperation(plan.OperationUpdate) {
+		for _, ds := range namespace.Datastore {
+			resourceSpec, err := c.resourceSpecReadWriter.ReadByName(".", plan.KindName)
+			if err != nil {
+				c.logger.Error(err.Error())
+				continue
+			}
+			resourceSpecProto, err := resourceSpec.ToProto()
+			if err != nil {
+				c.logger.Error(err.Error())
+				continue
+			}
+			resourcesToBeUpdate = append(resourcesToBeUpdate, &pb.UpdateResourceRequest{
+				ProjectName:   projectName,
+				NamespaceName: namespace.Name,
+				DatastoreName: ds.Type,
+				Resource:      resourceSpecProto,
+			})
+		}
+	}
+	return resourcesToBeUpdate
+}
+
+func (c *applyCommand) getDeleteResourceRequest(projectName string, namespace *config.Namespace, plans plan.Plans) []*pb.DeleteResourceRequest {
+	resourcesToBeDelete := []*pb.DeleteResourceRequest{}
+	for _, plan := range plans.GetByNamespaceName(namespace.Name).GetByKind(plan.KindResource).GetByOperation(plan.OperationDelete) {
+		for _, ds := range namespace.Datastore {
+			resourceSpec, err := c.resourceSpecReadWriter.ReadByName(".", plan.KindName)
+			if err != nil {
+				c.logger.Error(err.Error())
+				continue
+			}
+			resourcesToBeDelete = append(resourcesToBeDelete, &pb.DeleteResourceRequest{
+				ProjectName:   projectName,
+				NamespaceName: namespace.Name,
+				DatastoreName: ds.Type,
+				ResourceName:  resourceSpec.Name,
+				Force:         false,
+			})
+		}
+	}
+	return resourcesToBeDelete
+}
+
+func (c *applyCommand) getPlans() (plan.Plans, error) {
+	var plans plan.Plans = []*plan.Plan{}
+	for _, source := range c.sources {
+		f, err := os.OpenFile(source, os.O_RDONLY, os.ModePerm)
+		if err != nil {
+			return nil, err
+		}
+		defer f.Close()
+
+		currentPlans := []*plan.Plan{}
+		if err := gocsv.UnmarshalFile(f, &currentPlans); err != nil {
+			return nil, err
+		}
+		plans = append(plans, currentPlans...)
+	}
+	return plans, nil
+}
+
+func (c *applyCommand) savePlans(plans plan.Plans) error {
+	f, err := os.OpenFile(c.output, os.O_RDWR|os.O_CREATE|os.O_TRUNC, os.ModePerm)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	return gocsv.MarshalFile(plans, f)
 }
--- client/cmd/internal/plan/plan.go
+++ client/cmd/internal/plan/plan.go
@@ -12,3 +12,49 @@ type Plan struct {
 type Plans []*Plan
 
 func (p Plans) SortByOperationPriority(i, j int) bool { return p[i].Operation < p[j].Operation }
+
+func (p Plans) GetByKind(kind Kind) Plans {
+	return getBy(p, func(p *Plan) bool {
+		return p.Kind == kind
+	})
+}
+
+func (p Plans) GetByProjectName(projectName string) Plans {
+	return getBy(p, func(plan *Plan) bool {
+		return plan.ProjectName == projectName
+	})
+}
+
+func (p Plans) GetByNamespaceName(namespaceName string) Plans {
+	return getBy(p, func(plan *Plan) bool {
+		return plan.NamespaceName == namespaceName
+	})
+}
+
+func (p Plans) GetByOperation(operation Operation) Plans {
+	return getBy(p, func(plan *Plan) bool {
+		return plan.Operation == operation
+	})
+}
+
+func (p Plans) UpdateExecutedByNames(executed bool, kind Kind, kindNames ...string) {
+	mp := map[string]bool{}
+	for _, name := range kindNames {
+		mp[name] = true
+	}
+	for i, plan := range p {
+		if _, ok := mp[plan.KindName]; ok && plan.Kind == kind {
+			p[i].Executed = executed
+		}
+	}
+}
+
+func getBy(plans Plans, filter func(*Plan) bool) Plans {
+	result := []*Plan{}
+	for _, plan := range plans {
+		if filter(plan) {
+			result = append(result, plan)
+		}
+	}
+	return result
+}

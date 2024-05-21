76bfa548be176977859c6c5db5e11352bc60d7ff
okysetiawan <okyng08@gmail.com>
2024-05-21 17:58:25 +0700

    refactor: remove compositor from plan and move to plans

--- client/cmd/internal/plan/plan_test.go
+++ client/cmd/internal/plan/plan_test.go
@@ -8,7 +8,7 @@ import (
 	"github.com/goto/optimus/client/cmd/internal/plan"
 )
 
-func TestPlanCompositor(t *testing.T) {
+func TestPlanMerge(t *testing.T) {
 	var (
 		projectName = "p-optimus-1"
 		job1        = "j-job-1"
@@ -17,7 +17,7 @@ func TestPlanCompositor(t *testing.T) {
 	)
 
 	t.Run("case migration only", func(t *testing.T) {
-		inputs := []*plan.Plan{
+		inputs := plan.Plans{
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationDelete, KindName: job1},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationCreate, KindName: job1},
 		}
@@ -25,17 +25,12 @@ func TestPlanCompositor(t *testing.T) {
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationMigrate, KindName: job1, OldNamespaceName: &namespace1},
 		}
 
-		compositor := plan.NewCompositor()
-		for i := range inputs {
-			compositor.Add(inputs[i])
-		}
-
-		actual := compositor.Merge()
+		actual := inputs.Merge()
 		assert.ElementsMatch(t, actual, expected)
 	})
 
 	t.Run("case create update delete on different kind name and namespace", func(t *testing.T) {
-		inputs := []*plan.Plan{
+		inputs := plan.Plans{
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationCreate, KindName: "job-1"},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationUpdate, KindName: "job-2"},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationDelete, KindName: "job-3"},
@@ -52,18 +47,13 @@ func TestPlanCompositor(t *testing.T) {
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationDelete, KindName: "job-3"},
 		}
 
-		compositor := plan.NewCompositor()
-		for i := range inputs {
-			compositor.Add(inputs[i])
-		}
-
-		actual := compositor.Merge()
+		actual := inputs.Merge()
 		assert.Len(t, actual, len(expected), "actual has %d length, but expect has %d length", len(actual), len(expected))
 		assert.ElementsMatch(t, actual, expected)
 	})
 
 	t.Run("case migration from create to delete", func(t *testing.T) {
-		inputs := []*plan.Plan{
+		inputs := plan.Plans{
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationCreate, KindName: "job-1"},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationUpdate, KindName: "job-2"},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationDelete, KindName: "job-1"},
@@ -77,17 +67,12 @@ func TestPlanCompositor(t *testing.T) {
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationCreate, KindName: "job-3"},
 		}
 
-		compositor := plan.NewCompositor()
-		for i := range inputs {
-			compositor.Add(inputs[i])
-		}
-
-		actual := compositor.Merge()
+		actual := inputs.Merge()
 		assert.ElementsMatch(t, actual, expected)
 	})
 
 	t.Run("case migration from delete to create", func(t *testing.T) {
-		inputs := []*plan.Plan{
+		inputs := plan.Plans{
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationDelete, KindName: "job-1"},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationUpdate, KindName: "job-2"},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationCreate, KindName: "job-1"},
@@ -101,18 +86,13 @@ func TestPlanCompositor(t *testing.T) {
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationCreate, KindName: "job-3"},
 		}
 
-		compositor := plan.NewCompositor()
-		for i := range inputs {
-			compositor.Add(inputs[i])
-		}
-
-		actual := compositor.Merge()
+		actual := inputs.Merge()
 		assert.ElementsMatch(t, actual, expected)
 	})
 
 	t.Run("case multiple migration on multiple namespace", func(t *testing.T) {
 		namespace3, namespace4, namespace5 := "n-optimus-3", "n-optimus-4", "n-optimus-5"
-		inputs := []*plan.Plan{
+		inputs := plan.Plans{
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace1, Operation: plan.OperationDelete, KindName: job1},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace2, Operation: plan.OperationCreate, KindName: job1},
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace3, Operation: plan.OperationCreate, KindName: job1},
@@ -125,15 +105,7 @@ func TestPlanCompositor(t *testing.T) {
 			{Kind: plan.KindJob, ProjectName: projectName, NamespaceName: namespace5, Operation: plan.OperationCreate, KindName: job1},
 		}
 
-		compositor := plan.NewCompositor()
-		for i := range inputs {
-			compositor.Add(inputs[i])
-		}
-
-		actual := compositor.Merge()
+		actual := inputs.Merge()
 		assert.ElementsMatch(t, actual, expected)
-
-		// - make sure the actual result still same when its called multiple times
-		assert.ElementsMatch(t, compositor.Merge(), expected)
 	})
 }

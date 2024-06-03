8b4fdfedb8ce0a14fab502d1f4be3b0e234748dd
okysetiawan <okyng08@gmail.com>
2024-06-03 10:55:46 +0700

    refactor: logic append plan on save file

--- client/cmd/resource/plan.go
+++ client/cmd/resource/plan.go
@@ -241,7 +241,7 @@ func (p *planCommand) savePlan(plans plan.Plan) error {
 	existingBytes, _ := io.ReadAll(file)
 	var existingPlan plan.Plan
 	_ = json.Unmarshal(existingBytes, &existingBytes)
-	if existingPlan.SameProjectName(plans) && plans.Job.IsZero() {
+	if existingPlan.SameProjectName(plans) && !existingPlan.Job.IsZero() {
 		plans.Job = existingPlan.Job
 	}
 

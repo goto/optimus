5f7516e9810fa08d31fe420c77f2b845ea4ad8ad
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2024-06-06 15:14:34 +0700

    fix: reading from plan and write the summary

--- client/cmd/internal/plan/plan.go
+++ client/cmd/internal/plan/plan.go
@@ -15,13 +15,14 @@ func (p Plan) GetResult() Plan {
 }
 
 func (p Plan) Merge(otherPlan Plan) Plan {
-	if !p.SameProjectName(otherPlan) {
-		return p
-	}
 	newPlan := NewPlan(p.ProjectName)
 	newPlan.Job = p.Job.merge(otherPlan.Job)
 	newPlan.Resource = p.Resource.merge(otherPlan.Resource)
 	return newPlan
+}
+
+func (p Plan) IsEmpty() bool {
+	return p.ProjectName == "" && p.Job.IsZero() && p.Resource.IsZero()
 }
 
 func (p Plan) SameProjectName(compare Plan) bool { return p.ProjectName == compare.ProjectName }

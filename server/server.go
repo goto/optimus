29da1677332768b46ae1ea8bd4a8a530a7e6fd99
Oky Setiawan <49467802+okysetiawan@users.noreply.github.com>
2024-06-06 16:24:11 +0700

    feat: add keepalive enforcement policy 2m on server side (#233)
    * feat: add keepalive enforcement policy 2m on server side
    
    * fix: linter magic number on config keepalive enforcement policy

--- server/server.go
+++ server/server.go
@@ -38,6 +38,8 @@ const (
 
 	DialTimeout      = time.Second * 5
 	BootstrapTimeout = time.Second * 10
+
+	GRPCKeepaliveMinTime = 2 * time.Minute
 )
 
 func checkRequiredConfigs(conf config.Serve) error {
@@ -94,6 +96,10 @@ func setupGRPCServer(l log.Logger) (*grpc.Server, error) {
 		),
 		grpc.MaxRecvMsgSize(GRPCMaxRecvMsgSize),
 		grpc.MaxSendMsgSize(GRPCMaxSendMsgSize),
+		grpc.KeepaliveEnforcementPolicy(keepalive.EnforcementPolicy{
+			MinTime:             GRPCKeepaliveMinTime, // MinTime client should wait until next ping as in client side send ping every 1 Minute
+			PermitWithoutStream: true,                 // PermitWithoutStream send ping even without active stream
+		}),
 		grpc.KeepaliveParams(keepalive.ServerParameters{
 			Time:    1 * time.Minute, // Ping the client if it is idle for 1 minute to ensure the connection is still active
 			Timeout: 1 * time.Second, // Wait 1 second for the ping ack before assuming the connection is dead

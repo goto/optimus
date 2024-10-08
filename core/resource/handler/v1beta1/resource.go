5ef21244ddc4979fd1d9c21b6386e70fafd6992f
Ahmad N. F. <fahmadnaufal@gmail.com>
2024-10-08 16:42:32 +0700

    feat: restore empty name checking

--- core/resource/handler/v1beta1/resource.go
+++ core/resource/handler/v1beta1/resource.go
@@ -435,6 +435,9 @@ func (rh ResourceHandler) GetResourceChangelogs(ctx context.Context, req *pb.Get
 	}
 
 	resourceName := resource.Name(req.GetResourceName())
+	if resourceName == "" {
+		return nil, errors.GRPCErr(errors.InvalidArgument(resource.EntityResource, "resource name is empty"))
+	}
 
 	changelogs, err := rh.changelogService.GetChangelogs(ctx, projectName, resourceName)
 	if err != nil {

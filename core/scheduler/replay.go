b5d007ed6fe38c6d1342212679b5ebbb78a1e6ff
Yash Bhardwaj <imyashbhardwaj@gmail.com>
2024-10-18 15:10:15 +0530

    Handle replay across Optimus server restarts (#279)
    * fix: watch replay across restarts
    
    * fix: add test

--- core/scheduler/replay.go
+++ core/scheduler/replay.go
@@ -77,6 +77,7 @@ type Replay struct {
 	message string
 
 	createdAt time.Time
+	updatedAt time.Time
 }
 
 func (r *Replay) ID() uuid.UUID {
@@ -89,6 +90,10 @@ func (r *Replay) JobName() JobName {
 
 func (r *Replay) Tenant() tenant.Tenant {
 	return r.tenant
+}
+
+func (r *Replay) UpdatedAt() time.Time {
+	return r.updatedAt
 }
 
 func (r *Replay) Config() *ReplayConfig {
@@ -120,8 +125,8 @@ func NewReplayRequest(jobName JobName, tenant tenant.Tenant, config *ReplayConfi
 	return &Replay{jobName: jobName, tenant: tenant, config: config, state: state}
 }
 
-func NewReplay(id uuid.UUID, jobName JobName, tenant tenant.Tenant, config *ReplayConfig, state ReplayState, createdAt time.Time, message string) *Replay {
-	return &Replay{id: id, jobName: jobName, tenant: tenant, config: config, state: state, createdAt: createdAt, message: message}
+func NewReplay(id uuid.UUID, jobName JobName, tenant tenant.Tenant, config *ReplayConfig, state ReplayState, createdAt, updatedAt time.Time, message string) *Replay {
+	return &Replay{id: id, jobName: jobName, tenant: tenant, config: config, state: state, createdAt: createdAt, updatedAt: updatedAt, message: message}
 }
 
 type ReplayWithRun struct {

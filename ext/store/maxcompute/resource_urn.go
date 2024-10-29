b6ead53abc71f5c907223c12388e52a5f397992f
Sandeep Bhardwaj <sandeep.bhardwaj2307@gmail.com>
2024-10-29 13:37:55 +0530

    feat: add resource management support for maxcompute (#282)
    * feat: add initial resource mapping for maxcompute table
    
    * feat: improve validation, add table spec
    
    * tests: add test for table_spec and properties
    
    * feat: add code to create table on maxcompute
    
    * feat: initial setup of maxcompute store
    
    * feat: add maxcompute client and register maxcompute store
    
    * fix: change table handler initialization parameter type
    
    * fix: fix error on missing partition
    
    * fix: fix parameter type interface and handling error creation properly
    
    * fix: change partition column handling
    
    * test: add test for maxcompute client and table implementation
    
    * feat: implement checking existing table schema
    
    * chore: sort imports
    
    # Conflicts:
    #	ext/store/maxcompute/maxcompute.go
    #	ext/store/maxcompute/table.go
    #	ext/store/maxcompute/table_test.go
    
    * feat: add support for views in maxcompute
    
    * feat: connect support for views
    
    * feat: implement standard table updation and improve view table
    
    * feat: add clustering support for table creation and update view
    
    * feat: implement alter table column with json type
    
    * test: add cluster table and view test
    
    * feat: implement schema format temporary for resource urn
    
    * chore: fix lint errors
    
    * chore: restructure file
    
    * chore: update golangci-lint action
    
    * feat: incorporate version 2 changes for maxcompute
    
    * update name to be used in create and update
    
    * fix: fix failure for decode
    
    * fix test and list issues
    
    * fix: handle name for view updation
    
    * test: fix the error on maxcompute test handle form
    
    * test: fix the error on maxcompute view handle form mock
    
    ---------
    
    Co-authored-by: Frederick Wijayadi Susilo <frederick.susilo@gopay.co.id>
    Co-authored-by: Frederick Wijayadi Susilo <frederick.wijayadisusilo@gmail.com>

---
+++ ext/store/maxcompute/resource_urn.go
@@ -0,0 +1,140 @@
+package maxcompute
+
+import (
+	"fmt"
+	"strings"
+
+	"github.com/mitchellh/mapstructure"
+
+	"github.com/goto/optimus/core/resource"
+	"github.com/goto/optimus/internal/errors"
+)
+
+type ResourceURN struct {
+	Project string `mapstructure:"project"`
+	Schema  string `mapstructure:"database"`
+	Name    string `mapstructure:"name"`
+}
+
+func NewResourceURN(project, schema, name string) (ResourceURN, error) {
+	me := errors.NewMultiError("resource urn constructor errors")
+	if project == "" {
+		me.Append(fmt.Errorf("project is empty"))
+	}
+	if schema == "" {
+		me.Append(fmt.Errorf("schema is empty"))
+	}
+	if name == "" {
+		me.Append(fmt.Errorf("name is empty"))
+	}
+
+	if len(me.Errors) > 0 {
+		return ResourceURN{}, me.ToErr()
+	}
+
+	return ResourceURN{
+		Project: project,
+		Schema:  schema,
+		Name:    name,
+	}, nil
+}
+
+func (n ResourceURN) URN() string {
+	return "maxcompute://" + fmt.Sprintf("%s.%s.%s", n.Project, n.Schema, n.Name)
+}
+
+type ResourceURNWithUpstreams struct {
+	ResourceURN ResourceURN
+	Upstreams   []*ResourceURNWithUpstreams
+}
+
+type ResourceURNWithUpstreamsList []*ResourceURNWithUpstreams
+
+func (rs ResourceURNWithUpstreamsList) FlattenUnique() []*ResourceURNWithUpstreams {
+	var output []*ResourceURNWithUpstreams
+	for _, r := range rs {
+		if r == nil {
+			continue
+		}
+		newResource := *r
+		newResource.Upstreams = nil
+		nested := ResourceURNWithUpstreamsList(r.Upstreams).FlattenUnique()
+		output = append(output, &newResource)
+		output = append(output, nested...)
+	}
+
+	return ResourceURNWithUpstreamsList(output).unique()
+}
+
+func (rs ResourceURNWithUpstreamsList) unique() ResourceURNWithUpstreamsList {
+	mapUnique := map[ResourceURN]*ResourceURNWithUpstreams{}
+	for _, r := range rs {
+		mapUnique[r.ResourceURN] = r
+	}
+
+	output := make([]*ResourceURNWithUpstreams, len(mapUnique))
+	i := 0
+	for _, u := range mapUnique {
+		output[i] = u
+		i++
+	}
+	return output
+}
+
+type ProjectSchema struct {
+	Project string
+	Schema  string
+}
+
+type ResourceURNs []ResourceURN
+
+func (n ResourceURNs) GroupByProjectschema() map[ProjectSchema][]string {
+	output := make(map[ProjectSchema][]string)
+
+	for _, resourceURN := range n {
+		pd := ProjectSchema{Project: resourceURN.Project, Schema: resourceURN.Schema}
+		if _, ok := output[pd]; !ok {
+			output[pd] = []string{}
+		}
+		output[pd] = append(output[pd], resourceURN.Name)
+	}
+
+	return output
+}
+
+func URNFor(res *resource.Resource) (resource.URN, error) {
+	spec, err := getURNComponent(res)
+	if err != nil {
+		return resource.ZeroURN(), errors.InvalidArgument(resource.EntityResource, "not able to decode spec")
+	}
+
+	name := spec.Project + "." + spec.Schema + "." + spec.Name
+
+	return resource.NewURN(resource.MaxCompute.String(), name)
+}
+
+func getURNComponent(res *resource.Resource) (ResourceURN, error) {
+	var spec ResourceURN
+	if err := mapstructure.Decode(res.Spec(), &spec); err != nil {
+		return spec, err
+	}
+
+	return spec, nil
+}
+
+func getComponentName(res *resource.Resource) (resource.Name, error) {
+	component, err := getURNComponent(res)
+	if err != nil {
+		return "", err
+	}
+	return resource.Name(component.Name), nil
+}
+
+func resourceNameFor(name resource.Name) (string, error) {
+	parts := strings.Split(name.String(), ".")
+	if len(parts) < TableNameSections {
+		return "", errors.InvalidArgument(resource.EntityResource, "invalid resource name: "+name.String())
+	}
+
+	return parts[2], nil
+}

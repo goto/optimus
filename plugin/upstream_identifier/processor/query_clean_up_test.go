5ab246ccfca8d200ceeb7db0232c072170a84475
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2024-11-18 11:37:35 +0700

    test: add test for query clean up

--- plugin/upstream_identifier/processor/query_clean_up_test.go
+++ plugin/upstream_identifier/processor/query_clean_up_test.go
@@ -1,3 +1,31 @@
 package processor_test
 
-// TODO: Add tests for the CleanQuery function
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+
+	"github.com/goto/optimus/plugin/upstream_identifier/processor"
+)
+
+func TestCleanQueryFromComment(t *testing.T) {
+	query := `SELECT * FROM /* @ignoreupstream */ table -- single line comment
+	/* multi
+	line
+	comment */`
+	expected := `SELECT * FROM /* @ignoreupstream */ table 
+	`
+	cleanedQuery := processor.CleanQueryFromComment(query)
+	assert.Equal(t, expected, cleanedQuery)
+}
+
+func TestCleanQuery(t *testing.T) {
+	query := `SELECT * FROM /* @ignoreupstream */ table -- single line comment
+	/* multi
+	line
+	comment */`
+	expected := `SELECT * FROM  table 
+	`
+	cleanedQuery := processor.CleanQuery(query)
+	assert.Equal(t, expected, cleanedQuery)
+}

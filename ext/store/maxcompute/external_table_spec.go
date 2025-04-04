63a6c9d1aed75a96e294c75ececbbf9fc8b382af
Yash Bhardwaj <imyashbhardwaj@gmail.com>
2025-04-04 11:39:51 +0530

    feat: add support for formated data

--- ext/store/maxcompute/external_table_spec.go
+++ ext/store/maxcompute/external_table_spec.go
@@ -55,6 +55,7 @@ type ExternalSource struct {
 
 	SyncInterval     int64    `mapstructure:"sync_interval_in_hrs,omitempty"`
 	GetFormattedDate bool     `mapstructure:"fetch_formatted_datetime,omitempty"`
+	GetFormattedData bool     `mapstructure:"fetch_formatted_data,omitempty"`
 	Jars             []string `mapstructure:"jars,omitempty"`
 	Location         string   `mapstructure:"location,omitempty"`
 	Range            string   `mapstructure:"range,omitempty"`

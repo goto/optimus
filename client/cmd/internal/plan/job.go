4893a89e717fe5e38990033f0657d1cdd94e9cef
Ahmad N. F. <fahmadnaufal@gmail.com>
2025-05-06 16:17:18 +0700

    chore: fail optimus plan if yaml is malformed (#393)

--- client/cmd/internal/plan/job.go
+++ client/cmd/internal/plan/job.go
@@ -5,6 +5,7 @@ import "strings"
 type JobPlan struct {
 	Name         string  `json:"name"`
 	OldNamespace *string `json:"old_namespace"`
+	Path         string  `json:"path"`
 }
 
 func (p JobPlan) GetName() string { return p.Name }
@@ -12,6 +13,10 @@ func (p JobPlan) GetName() string { return p.Name }
 func (p *JobPlan) SetName(name string) { p.Name = name }
 
 func (p *JobPlan) SetOldNamespace(oldNamespace string) { p.OldNamespace = &oldNamespace }
+
+func (p JobPlan) GetPath() string { return p.Path }
+
+func (p *JobPlan) SetPath(path string) { p.Path = path }
 
 func (JobPlan) FileSuffix() string { return "/job.yaml" }
 

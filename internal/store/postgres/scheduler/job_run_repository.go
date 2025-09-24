b596b79d58cf000b361d739b8c3425d250e6e072
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2025-09-24 16:40:37 +0700

    feat: implement get p95 calculation from repo

--- internal/store/postgres/scheduler/job_run_repository.go
+++ internal/store/postgres/scheduler/job_run_repository.go
@@ -232,6 +232,48 @@ func (j *JobRunRepository) GetByScheduledTimes(ctx context.Context, t tenant.Ten
 	return jobRunList, nil
 }
 
+func (j *JobRunRepository) GetP95DurationByJobNames(ctx context.Context, jobNames []scheduler.JobName, lastNDays int) (map[scheduler.JobName]time.Duration, error) {
+	if len(jobNames) == 0 {
+		return map[scheduler.JobName]time.Duration{}, nil
+	}
+	var jobNamesString []string
+	for _, jobName := range jobNames {
+		jobNamesString = append(jobNamesString, fmt.Sprintf("'%s'", jobName))
+	}
+	query := `
+	SELECT
+	    j.job_name,
+	    percentile_cont(0.95) WITHIN GROUP (
+	        ORDER BY EXTRACT(EPOCH FROM (t.end_time - t.start_time))
+	    ) AS p95_duration_seconds
+	FROM job_run j
+	JOIN task_run t ON t.job_run_id = j.id
+	WHERE t.end_time IS NOT NULL
+	  AND j.scheduled_at >= NOW() - INTERVAL '%d days'
+	  AND j.job_name IN (%s)
+	GROUP BY j.job_name
+	ORDER BY p95_duration_seconds DESC;`
+	query = fmt.Sprintf(query, lastNDays, strings.Join(jobNamesString, ","))
+	rows, err := j.db.Query(ctx, query)
+	if err != nil {
+		return nil, errors.Wrap(scheduler.EntityJobRun, "error while getting job runs duration", err)
+	}
+	jobDurations := make(map[scheduler.JobName]time.Duration)
+	for rows.Next() {
+		var jobName string
+		var p95DurationSeconds float64
+		err := rows.Scan(&jobName, &p95DurationSeconds)
+		if err != nil {
+			if errors.Is(err, pgx.ErrNoRows) {
+				return map[scheduler.JobName]time.Duration{}, nil
+			}
+			return nil, errors.Wrap(scheduler.EntityJobRun, "error while getting job runs duration", err)
+		}
+		jobDurations[scheduler.JobName(jobName)] = time.Duration(p95DurationSeconds * float64(time.Second))
+	}
+	return jobDurations, nil
+}
+
 func (j *JobRunRepository) UpdateState(ctx context.Context, jobRunID uuid.UUID, status scheduler.State) error {
 	updateJobRun := "update job_run set status = $1, updated_at = NOW() where id = $2"
 	_, err := j.db.Exec(ctx, updateJobRun, status, jobRunID)

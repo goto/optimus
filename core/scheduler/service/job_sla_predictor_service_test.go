192991636ce2bb2f7a34cec78c1a77ac881a66e6
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2025-09-24 15:48:45 +0700

    test: add test cases to detect 1 potential breach given 2 jobs refering to same upstream

--- core/scheduler/service/job_sla_predictor_service_test.go
+++ core/scheduler/service/job_sla_predictor_service_test.go
@@ -67,7 +67,9 @@ func TestPredictJobSLAs(t *testing.T) {
 		jobBLineage.Upstreams = []*scheduler.JobLineageSummary{jobCLineage}
 		jobLineageFetcher.On("GetJobLineage", ctx, []*scheduler.JobSchedule{jobASchedule}).Return([]*scheduler.JobLineageSummary{jobALineage}, nil).Once()
 		// mock duration estimator
-		durationEstimator.On("GetP95DurationByJobNames", ctx, []scheduler.JobName{"job-A", "job-B", "job-C"}, 7).Return(map[scheduler.JobName]time.Duration{
+		durationEstimator.On("GetP95DurationByJobNames", ctx, mock.MatchedBy(func(jobNames []scheduler.JobName) bool {
+			return assert.ElementsMatch(t, []scheduler.JobName{"job-A", "job-B", "job-C"}, jobNames)
+		}), 7).Return(map[scheduler.JobName]time.Duration{
 			"job-A": 20 * time.Minute, // target sla now + 30 mins
 			"job-B": 15 * time.Minute, // inferred sla now + 10 mins
 			"job-C": 10 * time.Minute, // inferred sla now - 5 mins
@@ -141,7 +143,9 @@ func TestPredictJobSLAs(t *testing.T) {
 		jobBLineage.Upstreams = []*scheduler.JobLineageSummary{jobCLineage}
 		jobLineageFetcher.On("GetJobLineage", ctx, []*scheduler.JobSchedule{jobASchedule}).Return([]*scheduler.JobLineageSummary{jobALineage}, nil).Once()
 		// mock duration estimator
-		durationEstimator.On("GetP95DurationByJobNames", ctx, []scheduler.JobName{"job-A", "job-B", "job-C"}, 7).Return(map[scheduler.JobName]time.Duration{
+		durationEstimator.On("GetP95DurationByJobNames", ctx, mock.MatchedBy(func(jobNames []scheduler.JobName) bool {
+			return assert.ElementsMatch(t, []scheduler.JobName{"job-A", "job-B", "job-C"}, jobNames)
+		}), 7).Return(map[scheduler.JobName]time.Duration{
 			"job-A": 20 * time.Minute, // target sla now + 30 mins
 			"job-B": 15 * time.Minute, // inferred sla now + 10 mins
 			"job-C": 10 * time.Minute, // inferred sla now - 5 mins
@@ -215,7 +219,9 @@ func TestPredictJobSLAs(t *testing.T) {
 		jobBLineage.Upstreams = []*scheduler.JobLineageSummary{jobCLineage}
 		jobLineageFetcher.On("GetJobLineage", ctx, []*scheduler.JobSchedule{jobASchedule}).Return([]*scheduler.JobLineageSummary{jobALineage}, nil).Once()
 		// mock duration estimator
-		durationEstimator.On("GetP95DurationByJobNames", ctx, []scheduler.JobName{"job-A", "job-B", "job-C"}, 7).Return(map[scheduler.JobName]time.Duration{
+		durationEstimator.On("GetP95DurationByJobNames", ctx, mock.MatchedBy(func(jobNames []scheduler.JobName) bool {
+			return assert.ElementsMatch(t, []scheduler.JobName{"job-A", "job-B", "job-C"}, jobNames)
+		}), 7).Return(map[scheduler.JobName]time.Duration{
 			"job-A": 20 * time.Minute, // target sla now + 30 mins
 			"job-B": 15 * time.Minute, // inferred sla now + 10 mins
 			"job-C": 10 * time.Minute, // inferred sla now - 5 mins
@@ -227,6 +233,99 @@ func TestPredictJobSLAs(t *testing.T) {
 		assert.NoError(t, err)
 		assert.Len(t, jobLineages, 0)
 		assert.Len(t, jobToUpstreamMap, 0)
+	})
+	t.Run("given 2 jobs that refer to the same upstream job, and only 1 job that has potential breach, return 1 job lineages with its path", func(t *testing.T) {
+		// job-C -> job-B -> job-A1
+		//       -> job-A2
+		// job-A1 and job-A2 are the target jobs
+		// job-C is running late, so job-A1 might breach its SLA, but job-A2 is safe
+		// | job | estimated duration |
+		// |-----|--------------------|
+		// | A1  | 20 mins            | -> targeted SLA 30 mins from now
+		// | A2  | 25 mins            | -> targeted SLA 30 mins from now
+		// | B   | 15 mins            |
+		// | C   | 10 mins            | -> running late by 15 mins (actual run time 25 mins)
+		// given
+		jobLineageFetcher := new(MockJobLineageFetcher)
+		durationEstimator := new(MockDurationEstimator)
+		jobSLAPredictorService := service.NewJobSLAPredictorService(0*time.Minute, 7, jobLineageFetcher, durationEstimator)
+
+		now := time.Now().Truncate(time.Second)
+		targetedSLA := now.Add(30 * time.Minute)
+		jobA1 := scheduler.JobName("job-A1")
+		jobA2 := scheduler.JobName("job-A2")
+		jobB := scheduler.JobName("job-B")
+		jobC := scheduler.JobName("job-C")
+		// input job schedule
+		jobA1Schedule := &scheduler.JobSchedule{
+			JobName: jobA1,
+		}
+		jobA2Schedule := &scheduler.JobSchedule{
+			JobName: jobA2,
+		}
+		// mock job lineage fetcher
+		jobA1Lineage := &scheduler.JobLineageSummary{
+			JobName: jobA1,
+			JobRuns: map[string]*scheduler.JobRunSummary{
+				"job-A1": {
+					ScheduledAt: now,
+				},
+			},
+		}
+		jobA2Lineage := &scheduler.JobLineageSummary{
+			JobName: jobA2,
+			JobRuns: map[string]*scheduler.JobRunSummary{
+				"job-A2": {
+					ScheduledAt: now,
+				},
+			},
+		}
+		jobBLineage := &scheduler.JobLineageSummary{
+			JobName: jobB,
+			JobRuns: map[string]*scheduler.JobRunSummary{
+				"job-A1": {
+					ScheduledAt: now.Add(-15 * time.Minute),
+				},
+			},
+		}
+		jobCTaskStartTime := now.Add(-20 * time.Minute)
+		jobCLineage := &scheduler.JobLineageSummary{
+			JobName: jobC,
+			JobRuns: map[string]*scheduler.JobRunSummary{
+				"job-A1": {
+					ScheduledAt:   now.Add(-25 * time.Minute),
+					TaskStartTime: &jobCTaskStartTime, // job-C is running, but not done yet
+				},
+				"job-A2": {
+					ScheduledAt:   now.Add(-25 * time.Minute),
+					TaskStartTime: &jobCTaskStartTime, // job-C is running, but not done yet
+				},
+			},
+		}
+		jobA1Lineage.Upstreams = []*scheduler.JobLineageSummary{jobBLineage}
+		jobA2Lineage.Upstreams = []*scheduler.JobLineageSummary{jobCLineage}
+		jobBLineage.Upstreams = []*scheduler.JobLineageSummary{jobCLineage}
+		jobLineageFetcher.On("GetJobLineage", ctx, []*scheduler.JobSchedule{jobA1Schedule, jobA2Schedule}).Return([]*scheduler.JobLineageSummary{jobA1Lineage, jobA2Lineage}, nil).Once()
+		// mock duration estimator
+		durationEstimator.On("GetP95DurationByJobNames", ctx, mock.MatchedBy(func(jobNames []scheduler.JobName) bool {
+			return assert.ElementsMatch(t, []scheduler.JobName{"job-A1", "job-A2", "job-B", "job-C"}, jobNames)
+		}), 7).Return(map[scheduler.JobName]time.Duration{
+			"job-A1": 20 * time.Minute, // target sla now + 30 mins
+			"job-A2": 25 * time.Minute, // target sla now + 30 mins
+			"job-B":  15 * time.Minute, // inferred sla w.r.t job-A1 now + 10 mins
+			"job-C":  10 * time.Minute, // inferred sla w.r.t job-A1 now - 5 mins, w.r.t job-A2 now + 5 mins
+		}, nil).Once()
+
+		// when
+		jobLineages, jobToUpstreamMap, err := jobSLAPredictorService.PredictJobSLAs(ctx, []*scheduler.JobSchedule{jobA1Schedule, jobA2Schedule}, targetedSLA)
+		// then
+		assert.NoError(t, err)
+		assert.Len(t, jobLineages, 1)
+		assert.Len(t, jobToUpstreamMap, 1)
+		assert.Equal(t, jobA1Lineage, jobLineages[0])
+		assert.Equal(t, map[scheduler.JobName][]scheduler.JobName{
+			"job-A1": {"job-A1", "job-B", "job-C"},
+		}, jobToUpstreamMap)
 	})
 }
 

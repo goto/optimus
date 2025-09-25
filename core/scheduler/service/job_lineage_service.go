b4fa47403c1002c95d6ece444d54d59801dfeef1
Ahmad N. F. <fahmadnaufal@gmail.com>
2025-09-25 14:12:44 +0700

    complete implementation of build lineage

--- core/scheduler/service/job_lineage_service.go
+++ core/scheduler/service/job_lineage_service.go
@@ -20,6 +20,7 @@ type LineageBuilder interface {
 type JobLineageService struct {
 	l              log.Logger
 	lineageBuilder LineageBuilder
+	jobRunService  JobRunService
 }
 
 func (j *JobLineageService) GetJobExecutionSummary(ctx context.Context, jobSchedules []*scheduler.JobSchedule, numberOfUpstreamPerLevel int) ([]*scheduler.JobRunLineage, error) {
@@ -50,9 +51,11 @@ func (j *JobLineageService) GetJobLineage(ctx context.Context, jobSchedules []*s
 func NewJobLineageService(
 	l log.Logger,
 	lineageBuilder LineageBuilder,
+	jobRunService JobRunService,
 ) *JobLineageService {
 	return &JobLineageService{
 		l:              l,
 		lineageBuilder: lineageBuilder,
+		jobRunService:  jobRunService,
 	}
 }

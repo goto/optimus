9c01af4e5807b5d91e5a5622f033cd979f3d8ffa
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2025-10-20 15:18:23 +0700

    feat: persist third party type

--- core/scheduler/job.go
+++ core/scheduler/job.go
@@ -417,7 +417,6 @@ type ResourceConfig struct {
 type Upstreams struct {
 	HTTP         []*HTTPUpstreams
 	UpstreamJobs []*JobUpstream
-	ThirdParty   []*ThirdPartyUpstream
 }
 
 type HTTPUpstreams struct {
@@ -436,11 +435,8 @@ type JobUpstream struct {
 	Type           string
 	External       bool
 	State          string
-}
-
-type ThirdPartyUpstream struct {
-	Type string
-	URN  string
+	ResourceURN    string
+	ThirdPartyType string
 }
 
 type JobSummary struct {

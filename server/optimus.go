9f0e6d08e31db8ee05787cddf8a4a833b7ace76b
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2025-11-05 15:54:02 +0700

    fix: lint

--- server/optimus.go
+++ server/optimus.go
@@ -27,7 +27,6 @@ import (
 	rService "github.com/goto/optimus/core/resource/service"
 	schedulerHandler "github.com/goto/optimus/core/scheduler/handler/v1beta1"
 	schedulerResolver "github.com/goto/optimus/core/scheduler/resolver"
-	"github.com/goto/optimus/core/scheduler/service"
 	schedulerService "github.com/goto/optimus/core/scheduler/service"
 	tHandler "github.com/goto/optimus/core/tenant/handler/v1beta1"
 	tService "github.com/goto/optimus/core/tenant/service"
@@ -470,7 +469,7 @@ func (s *OptimusServer) setupHandlers() error {
 	// Resource Handler
 	pb.RegisterResourceServiceServer(s.grpcServer, rHandler.NewResourceHandler(s.logger, primaryResourceService, resourceChangeLogService))
 
-	sensorService := service.NewSensorService(s.logger, s.conf.UpstreamResolvers...)
+	sensorService := schedulerService.NewSensorService(s.logger, s.conf.UpstreamResolvers...)
 	pb.RegisterJobRunServiceServer(s.grpcServer, schedulerHandler.NewJobRunHandler(s.logger, newJobRunService, eventsService, newSchedulerService, jobLineageService, newJobSLAPredictorService, sensorService))
 
 	// backup service

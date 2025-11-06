5a4623562c7b68c815d7e31677c560db564c321c
Yash Bhardwaj <yash.bhardwaj@gojek.com>
2025-11-06 23:02:14 +0530

    fix: add test for bulk resolve

--- core/job/resolver/dex_upstream_resolver_test.go
+++ core/job/resolver/dex_upstream_resolver_test.go
@@ -35,13 +35,17 @@ func TestDexUpstreamResolver_Resolve(t *testing.T) {
 	jobTask := job.NewTask(taskName, jobTaskConfig, "", nil)
 	upstreamSpec, _ := job.NewSpecUpstreamBuilder().WithUpstreamNames([]job.SpecUpstreamName{"external-project/job-B"}).Build()
 	specA, _ := job.NewSpecBuilder(jobVersion, "job-A", "sample-owner", jobSchedule, jobWindow, jobTask).WithSpecUpstream(upstreamSpec).Build()
+	specB, _ := job.NewSpecBuilder(jobVersion, "job-B", "sample-owner", jobSchedule, jobWindow, jobTask).WithSpecUpstream(upstreamSpec).Build()
 	resourceURNC, err := resource.ParseURN("store://resource-C")
+	assert.NoError(t, err)
+	resourceURND, err := resource.ParseURN("store://resource-D")
 	assert.NoError(t, err)
 
 	resourceURNB, err := resource.ParseURN("store://resource-B")
 	assert.NoError(t, err)
 
 	jobA := job.NewJob(sampleTenant, specA, resource.ZeroURN(), []resource.URN{resourceURNC}, false)
+	jobB := job.NewJob(sampleTenant, specB, resource.ZeroURN(), []resource.URN{resourceURNC}, false)
 	t.Run("returns error, and bypass the resolved and unresolved upstreams as is, if tenant details getter fails", func(t *testing.T) {
 		logger := log.NewNoop()
 
@@ -261,6 +265,63 @@ func TestDexUpstreamResolver_Resolve(t *testing.T) {
 		assert.Len(t, result.Upstreams(), 2)
 		assert.Len(t, result.GetResolvedUpstreams(), 1)
 		assert.Len(t, result.GetUnresolvedUpstreams(), 1)
+	})
+
+	t.Run("bulk resolve", func(t *testing.T) {
+		unresolvedUpstreamC := job.NewUpstreamUnresolvedInferred(resourceURNC)
+		unresolvedUpstreamD := job.NewUpstreamUnresolvedInferred(resourceURND)
+		upstreamB := job.NewUpstreamResolved("job-B", "external-host", resourceURNB, sampleTenant, "static", taskName, true)
+		job1WithUnresolvedUpstream := job.NewWithUpstream(jobA, []*job.Upstream{upstreamB, unresolvedUpstreamC})
+
+		job2WithUnresolvedUpstream := job.NewWithUpstream(jobB, []*job.Upstream{upstreamB, unresolvedUpstreamD})
+
+		mockDexClient := new(mockThirdPartyClient)
+		mockDexClient.On("IsManaged", ctx, unresolvedUpstreamC.Resource()).Return(true, nil)
+		mockDexClient.On("IsManaged", ctx, unresolvedUpstreamD.Resource()).Return(false, nil)
+
+		defer mockDexClient.AssertExpectations(t)
+
+		project, err := tenant.NewProject(sampleTenant.ProjectName().String(), map[string]string{
+			"ENABLE_DEX_THIRD_PARTY_SENSOR": "1",
+			"STORAGE_PATH":                  "/data",
+			"SCHEDULER_HOST":                "external-host",
+		}, map[string]string{})
+
+		assert.NoError(t, err)
+		namespace, err := tenant.NewNamespace(sampleTenant.NamespaceName().String(), sampleTenant.ProjectName(), map[string]string{}, map[string]string{})
+		assert.NoError(t, err)
+		tenantWithDetails, err := tenant.NewTenantDetails(project, namespace, tenant.PlainTextSecrets{})
+		assert.NoError(t, err)
+
+		mockTenantGetter := new(mockTenantDetailsGetter)
+		mockTenantGetter.On("GetDetails", ctx, sampleTenant).Return(tenantWithDetails, nil)
+		defer mockTenantGetter.AssertExpectations(t)
+
+		dexUpstreamResolver := resolver.NewDexUpstreamResolver(logger, mockDexClient, mockTenantGetter)
+
+		logWriter := new(mockWriter)
+		defer logWriter.AssertExpectations(t)
+
+		jobsToBeResolved := []*job.WithUpstream{job1WithUnresolvedUpstream, job2WithUnresolvedUpstream}
+
+		result, err := dexUpstreamResolver.BulkResolve(ctx, jobsToBeResolved, logWriter)
+		assert.Nil(t, err)
+
+		assert.Len(t, result, 2)
+		for _, upstream := range result {
+			if upstream.Name() == "job-A" {
+				assert.Len(t, upstream.GetResolvedUpstreams(), 1)
+				assert.Equal(t, upstreamB, upstream.GetResolvedUpstreams()[0])
+				assert.Len(t, upstream.ThirdPartyUpstreams(), 1)
+				assert.Len(t, upstream.GetUnresolvedUpstreams(), 0)
+			}
+			if upstream.Name() == "job-B" {
+				assert.Len(t, upstream.GetResolvedUpstreams(), 1)
+				assert.Equal(t, upstreamB, upstream.GetResolvedUpstreams()[0])
+				assert.Len(t, upstream.ThirdPartyUpstreams(), 0)
+				assert.Len(t, upstream.GetUnresolvedUpstreams(), 1)
+			}
+		}
 	})
 }
 

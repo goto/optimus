5942abb89bf0297ec704a92c62aaaf75bb51f2d3
Dery Rahman Ahaddienata <dery.ra@gmail.com>
2025-11-27 06:29:18 +0700

    fix: running late detection

--- core/scheduler/service/job_sla_predictor_service.go
+++ core/scheduler/service/job_sla_predictor_service.go
@@ -473,7 +473,7 @@ func (s *JobSLAPredictorService) identifySLABreachRootCauses(jobTarget *schedule
 				break
 			}
 			// condition 1: T(now)>= S(u|j) and the job u has not completed yet
-			if (referenceTime.After(inferredSLA) && jobRun != nil && jobRun.JobEndTime == nil) || (jobRun != nil && jobRun.JobEndTime != nil && jobRun.JobEndTime.After(inferredSLA)) {
+			if (referenceTime.After(inferredSLA) && jobRun != nil && jobRun.JobEndTime == nil) || (jobRun != nil && jobRun.JobEndTime != nil) {
 				// found a job that might breach its SLA
 				potentialBreachPaths = append(potentialBreachPaths, paths)
 				// add to jobStateByName

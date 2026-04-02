b9b73889016e4f53765d3cca3744b9234b0e518f
Ahmad N. F. <fahmadnaufal@gmail.com>
2026-04-02 13:08:38 +0700

    add necessary columns & update repository

---
+++ core/audit/context.go
@@ -0,0 +1,22 @@
+package audit
+
+import "context"
+
+type ChangeOrigin struct {
+	Actor    string
+	Source   string
+	Metadata map[string]string
+}
+
+type contextKey struct{}
+
+func ToContext(ctx context.Context, origin ChangeOrigin) context.Context {
+	return context.WithValue(ctx, contextKey{}, origin)
+}
+
+func FromContext(ctx context.Context) ChangeOrigin {
+	if v, ok := ctx.Value(contextKey{}).(ChangeOrigin); ok {
+		return v
+	}
+	return ChangeOrigin{}
+}
